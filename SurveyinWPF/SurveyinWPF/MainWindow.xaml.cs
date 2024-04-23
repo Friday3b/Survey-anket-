60b7041ad3964961ce2b402f03c7855bac43580c
Taleh Haciyev <haciyev_99@internet.ru>
2024-04-24 01:13:45 +0400

    Survey
    here i have created survey that saving with JSON and fields of my class and XAML code binding each other and it went easyer for me to achieve some information about my code

---
+++ SurveyinWPF/SurveyinWPF/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.DirectoryServices.ActiveDirectory;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+
+
+
+namespace SurveyinWPF
+{
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //bunu burada yaratmaqin meqsedi local variable olmaqdan cixartmaq idi 
+        //eger initialize icersiinde yazsaydiq scope biten kimi buda bitecekdi ve ene ashaqida ishledemmiecekdik 
+
+        private User user;
+        
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            user = new User();
+            user.Nametxt = "Taleh"; 
+            DataContext = user;
+
+
+            
+
+        }
+
+        private void ConverttoJson_Click_Button(object sender, RoutedEventArgs e)
+        {
+            var filename = $"{user.Nametxt}";
+            var json = JsonSerializer.Serialize(user);
+            File.WriteAllText(filename, json);
+            MessageBox.Show("Json file already saved");
+        }
+
+
+        private void fromJson_ConvertFiletoScreen_button_click(object sender, RoutedEventArgs e)
+        {
+            var filename = $"{searchinpanel.Text}";
+
+            if (File.Exists(filename))
+            {
+                try
+                {
+                    var json = File.ReadAllText(filename);
+                    var loadedUser = JsonSerializer.Deserialize<User>(json);
+
+                    user.Nametxt = loadedUser.Nametxt;
+                    user.Surname = loadedUser.Surname;
+                    user.FatherName = loadedUser.FatherName;
+                    user.Country = loadedUser.Country;
+                    user.City = loadedUser.City;
+                    user.PhoneNumber = loadedUser.PhoneNumber;
+
+
+
+                    loadedUser.Nametxt = Namesection.Text;
+                    loadedUser.Surname=Surnamesection.Text;
+                    loadedUser.FatherName=FatherNameSection.Text;
+                    loadedUser.Country=Countrysection.Text;
+                    loadedUser.City=Citysection.Text;
+                    loadedUser.PhoneNumber=PhoneNumbersection.Text;
+
+
+
+
+                    //MessageBox.Show($"Name: {user.Nametxt}\n" +
+                    //                $"Surname: {user.Surname}\n" +
+                    //                $"FatherName: {user.FatherName}\n" +
+                    //                $"Country: {user.Country}\n" +
+                    //                $"City: {user.City}\n" +
+                    //                $"PhoneNumber: {user.PhoneNumber}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error reading json file: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Json file not found.");
+            }
+        }
+
+
+
+        //public class GenderConverter : IValueConverter
+        //{
+        //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        //    {
+        //        string genderValue = value as string;
+        //        string radioButtonContent = parameter as string;
+
+        //        return genderValue == radioButtonContent;
+        //    }
+
+        //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        //    {
+        //        bool isChecked = (bool)value;
+        //        string radioButtonContent = parameter as string;
+
+        //        if (isChecked)
+        //            return radioButtonContent;
+        //        else
+        //            return null;
+        //    }
+        //}
+
+
+
+    }
+}
---
+++ SurveyinWPF/SurveyinWPF/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SurveyinWPF
+{
+    public class User
+    {
+
+        public string Nametxt { get; set; } 
+        public string Surname { get; set; }
+        public string FatherName { get; set; }
+
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        //public Brush BackColor { get; set; } =
+        //new LinearGradientBrush(Colors.White, Colors.Blue, 45); 
+
+        //public DatePicker Birthday { get; set; }
+
+        public RadioButton Gender { get; set; }
+
+        //public ImageBrush BackgroundImage { get; set; } = new ImageBrush(new BitmapImage(new Uri("image/background/city.jpg" , UriKind.Relative)));
+
+
+        public override string ToString() => $" Name {Nametxt} Surname {Surname} FatherName{FatherName} " +
+            $" Country {Country} City{City} PhoneNumber{PhoneNumber}  ";
+
+        
+
+
+    }
+
+
+}
